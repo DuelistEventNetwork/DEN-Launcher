bac4296e4ba4fed2a84a03f37a6d018076e6b00b
axd1x8a <sign@axd1x8a.me>
2026-05-11 14:33:22 +0300

    make .env changes trigger recompile

--- build.rs
+++ build.rs
@@ -1,4 +1,5 @@
 fn main() {
+    println!("cargo:rerun-if-changed=.env");
     let mut res = winres::WindowsResource::new();
     res.set_icon("resources/logo.ico");
     if let Err(e) = res.compile() {
